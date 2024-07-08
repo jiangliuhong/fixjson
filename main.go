a7a8a35f13758f668974adc081eb5b2cc23228af
jiangliuhong <ja_rome@163.com>
2024-07-08 18:10:01 +0800

    修改配置

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"embed"
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -16,16 +18,24 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "fixjson",
+		Title:  "FixJSON",
 		Width:  1024,
 		Height: 768,
+		Menu:   menu.NewMenu(),
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		Frameless:                true,
+		EnableDefaultContextMenu: true,
+		BackgroundColour:         options.NewRGBA(27, 38, 54, 0),
+		OnStartup:                app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent:              true,
+			WindowIsTranslucent:               true,
+			DisableFramelessWindowDecorations: true,
 		},
 	})
 
